db83c93d5b68f251127e848c2792d63159e4c49b
Ryan Dawkes <ryandawkes7@gmail.com>
2020-10-10 18:52:48 +0100

    Camera Repostion

--- src/components/picture/pictures/sectionOnePic.js
+++ src/components/picture/pictures/sectionOnePic.js
@@ -127,7 +127,7 @@ function SectionOnePic(props) {
                     </div> :
                     <div className="camera-container">
                         <div className="image-overlay">
-                            <img src={PilotHelmet} alt="" style={{width: '350px'}} />
+                            <img src={PilotHelmet} alt="" style={{width: '300px'}} />
                         </div>
                         <Camera
                             onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
