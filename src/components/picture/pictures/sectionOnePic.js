f72f275bf1ac2b6d1154873a739d657af582d0ad
Ryan Dawkes <ryandawkes7@gmail.com>
2020-10-10 18:39:54 +0100

    Camera Img Resize

--- src/components/picture/pictures/sectionOnePic.js
+++ src/components/picture/pictures/sectionOnePic.js
@@ -127,7 +127,7 @@ function SectionOnePic(props) {
                     </div> :
                     <div className="camera-container">
                         <div className="image-overlay">
-                            <img src={PilotHelmet} alt="" style={{width: '25vw'}} />
+                            <img src={PilotHelmet} alt="" style={{width: '500px'}} />
                         </div>
                         <Camera
                             onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
